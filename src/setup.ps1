7c4a94364454390b5de5a934b956d373a7def238
librazhd7 <95590397+librazhd7@users.noreply.github.com>
2025-03-25 13:05:13 +0100

    Delete src/setup.ps1

--- src/setup.ps1
+++
@@ -1,4 +0,0 @@
-# fetch markdowns from this repo,
-# and translate them to htmls for later parsing of urls containing the desired .jars.
-# extract them to desired path after checking the validity of desired loader,
-# but in an isolated and non-conflicting way of already pre-existing instances.
